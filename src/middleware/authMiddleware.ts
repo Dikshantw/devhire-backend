65a4bc05a0924f59ca16fe77fb49a0d460fb109c
Dikshant Wavhule <dikshantw11@gmail.com>
2026-02-25 07:50:47 +0000

    add auth middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const jwtSecret = process.env.JWT_SECRET!;
+
+interface TokenPayload extends JwtPayload {
+    id: string;
+}
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization;
+    if(!header){
+        return res.status(401).json({message: "No Token provided"})
+    }
+    const token = header.replace("Bearer ","");
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret) as TokenPayload;
+
+        req.user = {
+            id: decoded.id
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({message: "Invalid token"})
+    }
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { login, signup } from "../auth/auth.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post('/signup',signup)
 router.post('/login',login)
-
+router.get('/me',authenticate, (req,res)=>{
+    res.json(req.user)
+})
 export default router;
