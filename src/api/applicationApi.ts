6cdc18588a3bed6fa475bcb3da5beeae15e4bde6
Dikshant Wavhule <dikshantw11@gmail.com>
2026-03-07 10:23:42 +0000

    feat: api to fetch applicants and update status

--- src/api/applicationApi.ts
+++ src/api/applicationApi.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { prisma } from "../lib/prisma";
+import { prisma } from "../lib/prisma.js";
 
 export const apply = async (req: Request, res:Response) => {
     const user = req.user;
@@ -25,4 +25,48 @@ export const apply = async (req: Request, res:Response) => {
         }
     })
     res.status(201).json(application)
+}
+
+export const getApplications = async (req:Request, res: Response) => {
+    const user = req.user;
+    const {jobId} = req.params;
+
+    const job = await prisma.job.findUnique({
+        where: {
+            id: jobId as string
+        }
+    })
+
+    if(!job){
+        throw new Error("Job not found")
+    }
+
+    const recruiter = await prisma.user.findUnique({
+        where: {
+            id: user?.id
+        }
+    })
+    if(job.companyId !== recruiter?.companyId){
+        throw new Error("Not authorized to view applicants for this job")
+    }
+
+    const applicants = await prisma.application.findMany({
+        where: {jobId: jobId as string},
+        include: {
+            user: true
+        }
+    })
+
+    res.status(200).json(applicants)
+}
+
+export const updateStatus = async(req: Request, res:Response) => {
+    const {applicationId} = req.params;
+    const {status} = req.body;
+
+    const updated = await prisma.application.update({
+        where: {id: applicationId as string},
+        data: { status }
+    })
+    res.status(200).json(updated)
 }
--- src/routes/applicationRoutes.ts
+++ src/routes/applicationRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/roleMiddleware.js";
-import { apply } from "../api/applicationApi.js";
+import { apply, getApplications, updateStatus } from "../api/applicationApi.js";
 
 const router = Router();
 
 router.post('/apply/:jobId',authenticate,authorize("SEEKER"),apply)
-
+router.get('/job/:jobId',authenticate,authorize("RECRUITER"),getApplications)
+router.patch('/:applicationId/status',authenticate,authorize("RECRUITER"),updateStatus)
 export default router;
